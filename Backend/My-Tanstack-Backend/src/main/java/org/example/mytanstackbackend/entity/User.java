caf2cd5d98fb8912c8e471a892b60cbe954acfaa
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-06-27 12:20:03 +0200

    user entity

---
+++ Backend/My-Tanstack-Backend/src/main/java/org/example/mytanstackbackend/entity/User.java
@@ -0,0 +1,105 @@
+package org.example.mytanstackbackend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Gender gender;
+
+    // Standard getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    // Default constructor
+    public User() {
+    }
+
+    // Constructor with parameters
+    public User(String firstName, String lastName, String email, String phoneNumber, Integer age, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public enum Gender {
+        MALE, FEMALE, OTHER
+    }
+}
