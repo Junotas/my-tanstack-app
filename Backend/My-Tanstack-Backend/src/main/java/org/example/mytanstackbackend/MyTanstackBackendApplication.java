133a1b6ebb99968ec798fdb3ca2f2dee7d167c6e
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-06-27 09:42:16 +0200

    setup for fullstack

---
+++ Backend/My-Tanstack-Backend/src/main/java/org/example/mytanstackbackend/MyTanstackBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.mytanstackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTanstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTanstackBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/My-Tanstack-Backend/src/test/java/org/example/mytanstackbackend/MyTanstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mytanstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTanstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
