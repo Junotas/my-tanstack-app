f699cf5fbd0d48edf486e024fd05619f4ac8af0c
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-07-08 08:32:14 +0200

    implemented user usercontroller and database config, still no database

--- Backend/src/main/java/org/example/backend/User.java
+++ Backend/src/main/java/org/example/backend/User.java
@@ -1,4 +1,16 @@
 package org.example.backend;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    // add getters and setters later
 }
